7eb22c381daedf0baa34d414bb92df0888e0cb3e
Dinesh <placeholder@example.com>
2025-02-07 17:48:25 +0530

    Initial commit - Adding project folder

---
+++ My.js
@@ -0,0 +1,16 @@
+document.getElementById("chess-link").addEventListener("click", function (e) {
+  console.log("Chess link clicked");
+  e.preventDefault();
+  window.open("https://www.chess.com", "_blank");
+});
+
+{
+  /* <section id="chess-section">
+  <iframe
+    src="https://www.chess.com"
+    width="100%"
+    height="800px"
+    frameborder="0"
+  ></iframe>
+</section>; */
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# FirstSite
+
+This is me adding my learnings of everyday Webdev into "my" site as a track of progress probably
